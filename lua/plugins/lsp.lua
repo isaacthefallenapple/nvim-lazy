170d74cc8d03e95d49abaf8e220205019b3150b7
isaacthefallenapple <isaacthefallenapple@gmail.com>
2023-02-17 08:07:56 +0100

    null-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,6 +15,20 @@ return {
             },
         },
     },
+
+    -- null-ls
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+            local nls = require("null-ls")
+            nls.setup({
+                debounce = 150,
+                sources = {
+                    nls.builtins.formatting.isort,
+                    nls.builtins.formatting.black,
+                },
+            })
+        end,
     },
   },
 
