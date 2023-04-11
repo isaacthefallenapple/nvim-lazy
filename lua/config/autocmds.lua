914052d7235ee58d4b37db704324c56b1eb28dd1
isaacthefallenapple <isaacthefallenapple@gmail.com>
2023-04-11 16:35:51 +0200

    add v-lang filetype

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,3 +9,11 @@ vim.api.nvim_create_autocmd('BufEnter', {
     group = format_group,
     callback = function() vim.opt.formatoptions = vim.opt.formatoptions - { 'o' } end,
 })
+
+local filetype_group = vim.api.nvim_create_augroup('Filetype', {})
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+    pattern = '*.v',
+    callback = function(_)
+        vim.o.filetype = 'vlang'
+    end,
+})
