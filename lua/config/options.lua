52858c4e8798081f37d93a96cccbc68084bb71d2
isaacthefallenapple <isaacthefallenapple@gmail.com>
2023-05-28 12:02:53 +0200

    add clipboard settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,16 @@ vim.opt.swapfile = false
 
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+
+vim.g.clipboard = {
+  name = "clipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
